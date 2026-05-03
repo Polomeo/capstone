5f6cebec3cea3cc54aa3a36623402e2fa5e7c04f
Martín <martin.g.lopez@gmail.com>
2026-05-03 18:48:34 -0300

    Removed unnecesary json import in api/views.py

--- capstone/api/views.py
+++ capstone/api/views.py
@@ -1,4 +1,3 @@
-import json
 from django.http import JsonResponse
 from django.shortcuts import render
 
@@ -6,7 +5,7 @@ from .models import Student
 
 # Create your views here.
 def students(request):
-    students = Student.objects.all().order_by('-last_name')
+    students = Student.objects.all().order_by('last_name')
 
     return JsonResponse({"students" : [student.serialize() for student in students]})
     
