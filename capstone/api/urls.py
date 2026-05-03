e9700141fffadac6bc588e84769e9c28aa539ccb
Martín <martin.g.lopez@gmail.com>
2026-05-03 16:01:03 -0300

    Created students view

--- capstone/api/urls.py
+++ capstone/api/urls.py
@@ -2,5 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.index, name="index"),
+    # API Routes
+    path('students', views.students, name="students"),
 ]
--- capstone/api/views.py
+++ capstone/api/views.py
@@ -1,5 +1,15 @@
+import json
+from django.http import JsonResponse
 from django.shortcuts import render
 
+from .models import Student
+
 # Create your views here.
-def index(request):
-    pass 
+def students(request):
+    students = Student.objects.all().order_by('-last_name')
+
+    if len(students) == 0:
+        return JsonResponse({"no_students" : "There are no students registered."})
+    else:
+        return JsonResponse({"students" : [student.serialize() for student in students],})
+    
