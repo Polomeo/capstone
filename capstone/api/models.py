566e0a15721970a4ff6e59a74e44ebde8f16fe10
Martín <martin.g.lopez@gmail.com>
2026-05-03 15:51:19 -0300

    Migrated Student model

--- capstone/api/models.py
+++ capstone/api/models.py
@@ -2,9 +2,6 @@ from django.contrib.auth.models import AbstractUser
 from django.db import models
 
 # Create your models here.
-class User(AbstractUser):
-    pass
-
 class Student(models.Model):
     
     first_name = models.CharField()
