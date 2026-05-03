65c0f7d55bfd6ade2280e96bc14ea817f37dbfcd
Martín <martin.g.lopez@gmail.com>
2026-05-03 15:48:39 -0300

    Added Student model

--- README.md
+++ README.md
@@ -1,2 +1,9 @@
 # capstone
 CS50 Submit Capstone
+
+# Technologies
+I dediced to use Django REST Api for the backend, since I won't be using templates from Django, and handle the front-end with React JavaScript Framework. This decoupling approach allows to an easier further mantenaice and grow.
+
+
+
+
--- capstone/api/models.py
+++ capstone/api/models.py
@@ -1,3 +1,26 @@
+from django.contrib.auth.models import AbstractUser
 from django.db import models
 
 # Create your models here.
+class User(AbstractUser):
+    pass
+
+class Student(models.Model):
+    
+    first_name = models.CharField()
+    last_name = models.CharField()
+    personal_id_number = models.CharField() # CharField since it won't be used to math
+    enroll_year = models.IntegerField()
+    enroll_id = models.IntegerField()
+
+    def serialize(self):
+        return {
+            'first_name' : self.first_name,
+            'last_name' : self.last_name,
+            'personal_id' : self.personal_id_number,
+            'enroll_year' : self.enroll_year,
+            'enroll_id' : self.enroll_id,
+        }
+
+    def __str__(self):
+        return f'{self.enroll_year} | {self.last_name}, {self.first_name}'
