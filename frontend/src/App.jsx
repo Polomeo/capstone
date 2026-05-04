cdc89885379a8bb2670e3ee8208915376ee70197
Martín <martin.g.lopez@gmail.com>
2026-05-03 22:58:07 -0300

    Router implemented. Uninstalled react-router-dom from capstone/ and installed in capstone/frontend/

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,13 +1,17 @@
+import {Routes, Route} from 'react-router-dom'
+
 import ExamsPage from "./pages/ExamsPage"
 import StudentsPage from "./pages/StudentsPage"
 
 function App() {
 
   return (
-    <>
-      <ExamsPage />
-      <StudentsPage />
-    </>
+    <main className='main-container'>
+      <Routes>
+        <Route path="/" element={<StudentsPage />} />
+        <Route path="/exams" element={<ExamsPage />} />
+      </Routes>
+    </main>
   )
 }
 
