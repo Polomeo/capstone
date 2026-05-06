d7c6a064ed51baf1094c98885767d3fc00bf3e0d
Polomeo <martin.g.lopez@gmail.com>
2026-05-06 12:55:12 -0300

    Added data validation to AddStudentForm

--- frontend/src/components/AddStudentForm.jsx
+++ frontend/src/components/AddStudentForm.jsx
@@ -10,33 +10,96 @@ function AddStudentForm() {
         setIsAddingStudent(!isAddingStudent);
     }
     
+    function handleEnroll(formData) {
+        // e.preventDefault();
+        
+        const sanitizedData = validateNewStudentData(formData)
+
+        if(sanitizedData.error){
+            alert("Error: " + sanitizedData.error);
+        }
+        else{
+            alert("Success:");
+        }
+    }
+
+    function validateNewStudentData(formData) {
+        
+        let sanitizedData = {}
+        
+        const lastName = formData.get("last_name");
+        const firstName = formData.get("first_name");
+        const personalIdNumber = parseInt(formData.get("personal_id_number"));
+        const enrollYear = parseInt(formData.get("enroll_year"));
+        const enrollId = parseInt(formData.get("enroll_id"));
+
+        // Sanitize Last name
+        if(lastName.length > 30 || lastName.length < 2)
+            {
+            sanitizedData["error"] = "Last name must be between 2 and 30 characters."
+        } else {
+            sanitizedData["lastName"] = lastName
+        }
+
+        // Sanitize Fist name
+        if(firstName.length > 30 || firstName.length < 2) {
+            sanitizedData["error"] = "First name must be between 2 and 30 characters."
+        } else {
+            sanitizedData["firstName"] = firstName
+        }
+
+        // Sanitize personal id number
+        if(personalIdNumber < 1000000
+            || personalIdNumber > 99999999) {
+            sanitizedData["error"] = "Personal ID number must be between 1.000.000 and 99.999.999";
+        }
+        else {
+            sanitizedData["personalIdNumber"] = personalIdNumber;
+        }
+
+        // Sanitize enroll year
+        if(enrollYear < 2020 || enrollYear > 2026){
+            sanitizedData["error"] = "Enroll Year must be between 2020 and 2026"
+        } else {
+            sanitizedData["enrollYear"] = enrollYear;
+        }
+
+        // Enroll id number could stay blank
+        sanitizedData["enrollId"] = enrollId
+
+        return sanitizedData
+    }
+
     return (
-        <form className="border border-info rounded" style={{padding: "10px", margin: "10px 0"}}>
+        <form action={handleEnroll} className="border border-info rounded" style={{padding: "10px", margin: "10px 0"}}>
             <div className="row">
                 <div className="form-group col-md-3">
                     <label>Last name</label>
-                    <input type="text" className="form-control" placeholder="Last name"/>
+                    <input name="last_name" type="text" className="form-control" placeholder="Last name" autoFocus/>
                 </div>
                 <div className="form-group col-md-3">
                     <label>First name</label>
-                    <input type="text" className="form-control" placeholder="First name" />
+                    <input name="first_name" type="text" className="form-control" placeholder="First name" />
                 </div>
                 <div className="form-group col-md-2">
                     <label>Personal ID Number</label>
-                    <input type="text" className="form-control" placeholder="11.222.333" />
+                    <input name="personal_id_number" type="number" className="form-control" placeholder="11.222.333" />
                 </div>
                 <div className="form-group col-md-2">
                     <label>Enroll Year</label>
-                    <input type="text" className="form-control" placeholder="2026" />
+                    <input name="enroll_year" type="number" className="form-control" placeholder="2026" />
                 </div>
                 <div className="form-group col-md-2">
                     <label>Enroll ID Number</label>
-                    <input type="text" className="form-control" placeholder="123" />
+                    <input name="enroll_id" type="number" className="form-control" placeholder="123" />
                 </div>
             </div>
             <div className="row" style={{padding: "10px"}}>
                 <div className="form-group col-md-2">
-                    <button type="submit" className="btn btn-primary">Enroll</button>
+                    <button 
+                        type="submit" 
+                        className="btn btn-primary"
+                        >Enroll</button>
                     <button 
                         className="btn btn-danger" 
                         style={{marginLeft: "10px"}}
