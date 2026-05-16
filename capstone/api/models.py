70a34058821260d53884f4c58095d7d1fd36af27
Martín <martin.g.lopez@gmail.com>
2026-05-16 16:14:11 -0300

    Created models for Subject, Exam and Grade

--- capstone/api/models.py
+++ capstone/api/models.py
@@ -22,3 +22,66 @@ class Student(models.Model):
 
     def __str__(self):
         return f'{self.enroll_year} | {self.last_name}, {self.first_name}'
+
+class Subject(models.Model):
+    name_full = models.CharField()
+    name_short = models.CharField()
+    course = models.IntegerField() # 1, 2, 3
+
+    def serialize(self):
+        return {
+            'id' : self.id,
+            'name_full' : self.name_full,
+            'name_short' : self.name_short,
+            'course' : self.course
+        }
+    
+    def __set__(self):
+        return f'{self.course}° | {self.name_short}'
+    
+class Exam(models.Model):
+    subject = models.ForeignKey(Subject,
+                                on_delete=models.CASCADE,
+                                related_name='subject_exams')
+    date = models.DateField()
+    filled = models.BooleanField(default=False)
+
+    def serialize(self):
+        return {
+            'id' : self.id,
+            'subject_full' : self.subject.name_short,
+            'subject_short' : self.subject.name_short,
+            'date' : self.date.strftime("%Y-%m-%d")
+        }
+    
+    def __set__(self):
+        return f"{self.date.strftime("%Y-%m-%d")}"
+    
+
+class Grade(models.Model):
+    student = models.ForeignKey(Student,
+                                on_delete=models.CASCADE,
+                                related_name='grades')
+    exam = models.ForeignKey(Exam,
+                             on_delete=models.CASCADE,
+                             related_name='grades')
+    grading = models.IntegerField(blank=True)
+    absent = models.BooleanField(default=False)
+
+    def serialize(self):
+        return {
+            'id': self.id,
+            'subject_id' : self.exam.subject.id,
+            'subject_short' : self.exam.subject.name_short,
+            'student_id' : self.student.id,
+            'student_full_name' : f'{self.student.last_name}, {self.student.first_name}',
+            'student_personal_id' : f'{self.student.personal_id_number}',
+            'exam_id' : self.exam.id,
+            'exam_date' : self.exam.date.strftime("%Y-%m-%d"),
+            'exam_grading' : self.grading,
+            'absent' : self.absent,
+        }
+    
+    def __str__(self):
+        final_grade  = "Absent" if self.absent else self.grading
+        return f"{self.exam.subject.name_short} | {self.student.last_name} | {final_grade}"
