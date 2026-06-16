5cfd4dad77429369cf7363162e72fc9e6e07ba59
Martín <martin.g.lopez@gmail.com>
2026-06-16 19:58:52 -0300

    In StudentGradingForm added Delete button for the student

--- frontend/src/components/StudentGradingForm.jsx
+++ frontend/src/components/StudentGradingForm.jsx
@@ -4,12 +4,21 @@ function StudentGradingForm({ StudentGradingData }){
     
     const [isAbsent, setIsAbsent] = useState(StudentGradingData.absent);
     const [grading, setGrading] = useState(StudentGradingData.exam_grading);
+    const [isDeleted, setIsDeleted] = useState(false);
 
     
     const handleAbsentChange = (event) => {
         // Blocks the Grading input
         // and sets the checkbox
         setIsAbsent(event.target.checked);
+    }
+
+    const handleDelete = (event) => {
+        // Sets styles to reflect student ready to be deleted
+        // Sets the hidden input "deleted_" to true
+        event.preventDefault();
+        setIsDeleted(!isDeleted);
+        // console.log("Deleted!");
     }
 
     
@@ -19,24 +28,28 @@ function StudentGradingForm({ StudentGradingData }){
             style={{maxWidth : "758px", width : "100%"}}
             >
             <div className="row g-2 align-items-center text-start">
-                <div className="col-6 col-md-3 fw-semibold">
+                <div 
+                    className="col-6 col-md-3 fw-semibold"
+                    style={{textDecoration: isDeleted ? "line-through" : "none", color: isDeleted ? "red" : "black"}}
+                    >
                     { StudentGradingData.student_full_name }
                 </div>
                 <div className="col-md-2">
                     <input type="number" 
                         className="form-control"
-                        name={ "grading_" + StudentGradingData.id } 
+                        name={ "grading_" + StudentGradingData.id }
+                        id= {"grading_" + StudentGradingData.id }
                         value={ grading ? grading : "" }
                         placeholder="Grading"
-                        disabled={ isAbsent }
-                        style={{textDecoration: isAbsent ? "line-through" : "none"}}
+                        disabled={ isAbsent || isDeleted }
+                        style={{textDecoration: isAbsent || isDeleted ? "line-through" : "none"}}
                         onChange={(e) => setGrading(e.target.value)}
                         min="1"
                         max="10"
                         step="1"
                     />
                 </div>
-                <div className="col-md-4">
+                <div className="col-md-2">
                     <input type="checkbox" 
                         className="form-check-input" 
                         id={"absent_" + StudentGradingData.id} 
@@ -46,9 +59,25 @@ function StudentGradingForm({ StudentGradingData }){
                     />
                     <label
                         className={`form-check-label ms-2 small ${isAbsent ? 'text-danger' : 'text-muted'}`}
+                        style={{textDecoration: isAbsent || isDeleted ? "line-through" : "none"}}
                         htmlFor={"absent_" + StudentGradingData.id} >
                         Absent
                     </label>
+                </div>
+                <div className="col-md-2">
+                    <button 
+                        type="button" 
+                        className={`btn btn-sm ${isDeleted ? 'btn-primary' : 'btn-danger'}`}
+                        onClick={handleDelete}
+                        >
+                        { isDeleted ? "Undo" : "Delete" }
+                    </button>
+                    <input 
+                        type="hidden" 
+                        id={"deleted_" + StudentGradingData.id }
+                        name={"deleted_" + StudentGradingData.id }
+                        value={isDeleted && isDeleted ? "true" : "false"}
+                    />
                 </div>
             </div>
         </div>
