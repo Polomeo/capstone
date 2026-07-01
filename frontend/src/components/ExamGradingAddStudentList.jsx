1d5286e25d4b6e9ea805344bee694fb1da11d11e
Martín <martin.g.lopez@gmail.com>
2026-07-01 15:55:01 -0300

    Added buttons to ExamGradingAddStudentList

--- frontend/src/components/ExamGradingAddStudentList.jsx
+++ frontend/src/components/ExamGradingAddStudentList.jsx
@@ -11,20 +11,40 @@ function ExamGradingAddStudentList({examId}){
         {'id': 4, 'student_full_name': "Abbot, Anna", 'already_in_exam': false, 'already_approved': false},
     ]
 
-    function handleAdd(e){
+    function handleCancelButton(event){
+        event.preventDefault();
+        console.log("Canceled.")
+    }
+    
+    function handleAddStudents(event){
         e.preventDefault();
         console.log("Students added.")
+
+        const formData = new FormData(event.currentTarget);
+
     }
 
     return (
         <form 
-            onSubmit={(e) => handleAdd(e)}
+            onSubmit={(e) => handleAddStudents(e)}
             className="form-inline"
         >
         <div>
             {studentData.map((student) => (
                 <ExamGradingAddStudentForm StudentData={student} key={student.id} />
             ))}
+        </div>
+        <div className="form-group col-md-4">
+            <button 
+                className="btn btn-danger"
+                onClick={(e) => handleCancelButton(e)}>
+            Cancel
+            </button>
+            <button
+                className="btn btn-primary"
+                type="submit">
+            Add students to exam
+            </button>
         </div>
         </form>
     )
