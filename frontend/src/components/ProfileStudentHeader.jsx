92915efb2d9f1f6d95f75754e3f44931aa64bb24
Martín <martin.g.lopez@gmail.com>
2026-07-06 14:14:27 -0300

    Fixed a bug in ProfilePage that prevented the student data to be drilled in ProfileStudentHeader: added a JavaScript shortcircuit to ensure the data is loaded before rendering the component

--- frontend/src/components/ProfileStudentHeader.jsx
+++ frontend/src/components/ProfileStudentHeader.jsx
@@ -9,25 +9,25 @@ function ProfileStudentHeader({ studentPersonalData }){
     }, [])
 
     return (
-        // <div className="container">
-        //     <div className="row">
-        //         <div className="col">
-        //             <h3>{personalData.last_name}, {personalData.first_name}</h3>
-        //         </div>
-        //     </div>
-        //     <div className="row">
-        //         <div className="col">
-        //             <h6>Enroll year: {personalData.enroll_year}</h6>
-        //         </div>
-        //         <div className="col">
-        //             <h6>Personal ID N°: {personalData.personal_id}</h6>
-        //         </div>
-        //         <div className="col">
-        //             <h6>Enroll ID N°: {personalData.enroll_id}</h6>
-        //         </div>
-        //     </div>
-        // </div>
-        <>Test</>
+        <div className="container">
+            <div className="row">
+                <div className="col">
+                    <h3>{personalData.last_name}, {personalData.first_name}</h3>
+                </div>
+            </div>
+            <div className="row">
+                <div className="col">
+                    <h6>Enroll year: {personalData.enroll_year}</h6>
+                </div>
+                <div className="col">
+                    <h6>Personal ID N°: {personalData.personal_id}</h6>
+                </div>
+                <div className="col">
+                    <h6>Enroll ID N°: {personalData.enroll_id}</h6>
+                </div>
+            </div>
+        </div>
+        // <>Test</>
     )
 }
 
--- frontend/src/pages/ProfilePage.jsx
+++ frontend/src/pages/ProfilePage.jsx
@@ -6,65 +6,25 @@ import ProfileStudentHeader from "../components/ProfileStudentHeader";
 function ProfilePage(){
     
     const { id } = useParams();
-    const [studentPersonalInfo, setStudentPersonalInfo] = useState({});
+    const [studentPersonalInfo, setStudentPersonalInfo] = useState(null);
     const [studentAcademicHistory, setStudentAcademicHistory] = useState([]);
 
     useEffect(() =>{
         fetch(`http://localhost:8000/api/profile_info/${id}`)
         .then(res => res.json())
         .then(data => {
-            // console.log(data);
-            console.log(data.student_personal_info);
-            console.log(data.student_academic_history);
-
             setStudentPersonalInfo(data.student_personal_info);
             setStudentAcademicHistory(data.student_academic_history);
         })
     }, [])
-
-    const studentPersonalDataTest = {
-        'id' : id,
-        'first_name' : 'Test Name',
-        'last_name' : 'Test LastName',
-        'personal_id' : '11222333',
-        'enroll_year' : '2025',
-        'enroll_id' : '125'
-    }
-
-    // const studentAcademicHistoryTest = [
-    //     {
-    //         'exam_id': 1, 
-    //         'subject_year' : "1", 
-    //         'subject_short': 'DIVINATION I', 
-    //         'exam_grading' : "7", 
-    //         'absent' : false, 
-    //         'exam_date' : '2025-04-01',
-    //     },
-    //     {
-    //         'exam_id': 2, 
-    //         'subject_year' : "1", 
-    //         'subject_short': 'DADA I', 
-    //         'exam_grading' : '', 
-    //         'absent' : true, 
-    //         'exam_date' : '2025-08-01',
-    //     },
-    //     {
-    //         'exam_id': 3, 
-    //         'subject_year' : "2", 
-    //         'subject_short': 'DADA II', 
-    //         'exam_grading' : '2',
-    //         'absent' : false, 
-    //         'exam_date' : '2026-04-02',
-    //     },
-    // ]
     
     return (
         <div className="container">
             <div>
-                <ProfileStudentHeader studentPersonalData={studentPersonalInfo} />
+                {(studentPersonalInfo) && <ProfileStudentHeader studentPersonalData={studentPersonalInfo} />}
             </div>
             <div>
-                <ProfileAcademicHistoryList studentAcademicHistory={studentAcademicHistory} />
+                {(studentAcademicHistory) && <ProfileAcademicHistoryList studentAcademicHistory={studentAcademicHistory} />}
             </div>
         </div>
     )
