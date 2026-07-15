2da6a05afa41ae928c75760d51ddb61a1c001a9d
Martín <martin.g.lopez@gmail.com>
2026-07-14 21:27:26 -0300

    Started delete exam

--- frontend/src/components/ExamGradingHeader.jsx
+++ frontend/src/components/ExamGradingHeader.jsx
@@ -4,6 +4,8 @@ import { useSearchParams } from "react-router-dom";
 function ExamGradingHeader({ examId }){
 
     const [examData, setExamData] = useState({});
+    const [isEditingExam, setIsEditingExam] = useState(false);
+    const [errors, setErrors] = useState([]);
 
     // Fetch the data
     useEffect(() => {
@@ -25,11 +27,29 @@ function ExamGradingHeader({ examId }){
         .catch(error => console.error('Error: ', error));
     }, [])
 
+    // Edit exam
+    function handleDeleteExam(event){
+        event.preventDefault();
+        console.log("Delete exam pressed.")
+    }
+    // Delete exam
+
+    // Cancel edit
+    function handleCancelButton(event){
+        event.preventDefault();
+        console.log("Delete canceled.")
+    }
+
     return(
-        <div>
-            <h1>{examData.subject_full}</h1>
-            <h6>Course: {examData.subject_course}° Year</h6>
-            <h6>{examData.date}</h6>
+        <div className="exam-header">
+            <div className="exam-data">
+                <h1>{examData.subject_full}</h1>
+                <h6>Course: {examData.subject_course}° Year</h6>
+                <h6>{examData.date}</h6>
+            </div>
+            <div className="exam-delete">
+                <h6>Are you sure you want to delete this exam? All gradings in this exam would be deleted.</h6>
+            </div>
         </div>
     )
 }
